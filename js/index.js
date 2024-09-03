8aa38cda49277a83bb80754b3a8ff9d6c5851486
kngan2201 <2351050111ngan@ou.edu.vn>
2024-09-03 11:19:25 +0700

    up

---
+++ js/index.js
@@ -0,0 +1,25 @@
+window.onload = function () {
+    //---------- FO TO TOP --------------------------------------------------------------------//
+        let goTop =document.getElementById("gotoTop")
+        goTop.onclick = function() {
+            //cuộn trang đến một phần tử cụ thể mượt hơn
+            document.documentElement.scrollIntoView({ 
+                behavior: 'smooth' 
+            });
+        }
+    
+    //---------- RESPONSIVE THANHMENU -----------------------------------------------------------//
+        let btn = document.querySelector("#menuBtn")
+        let thanhMenu = document.querySelector(".thanhmenu")
+        btn.onclick = function () {
+            thanhMenu.classList.toggle("show");
+            document.documentElement.scrollIntoView({ 
+                behavior: 'smooth' 
+            });
+    
+        }
+        let closemenu = document.getElementById("closeMenu")
+        closemenu.onclick = function () {
+            thanhMenu.classList.remove("show");
+        } 
+}    
